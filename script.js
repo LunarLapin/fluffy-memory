71cc51a58715dd623e31fcf1ae825f4ce3617c58
Kay42 <140291863+LunarLapin@users.noreply.github.com>
2024-11-01 19:53:55 -0400

    changed code to begin css practice

--- script.js
+++
@@ -1,9 +0,0 @@
-function clickFunction () {
-    const numberOne = document.getElementById("mathOne").value;
-    const numberTwo = document.getElementById("mathTwo").value;
-    
-    const sum = +numberOne + +numberTwo;
-    var total = sum.toString();
-    document.getElementById("result").textContent = total;
-    console.log(total); 
-}
